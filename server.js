649f0aab2cf22675e70b7f8b88959db481aa5ac4
Ahmed Jamlan <ahmedjamlan12@gmail.com>
2022-12-09 18:09:32 +0300

    renamed files and fixed page routes

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const port = 4000;
 
 app.use(express.static(__dirname + '/public'));
 express.urlencoded({extended: true});
+
 app.get('/',function(req,res){
     res.render('../views/index.ejs');
+});
+
+app.get('/profile',function(req,res){
+    res.render('../views/profile.ejs');
 });
 
 app.get('/home', function(req,res){
