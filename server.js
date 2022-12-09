89b79a1e848325f3d2c12574675344cdb0623c3f
Ahmed Jamlan <ahmedjamlan12@gmail.com>
2022-12-09 18:06:31 +0300

    Revamped the front-end with new styles and sections

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+const { urlencoded } = require('express');
 const express = require('express');
 const app = express();
 
 const port = 4000;
 
 app.use(express.static(__dirname + '/public'));
-
+express.urlencoded({extended: true});
 app.get('/',function(req,res){
     res.render('../views/index.ejs');
 });
+
+app.get('/home', function(req,res){
+    res.render('../views/home.ejs');
+})
 
 app.listen(port, function(){
     console.log(`Listneing on ${port}`);
